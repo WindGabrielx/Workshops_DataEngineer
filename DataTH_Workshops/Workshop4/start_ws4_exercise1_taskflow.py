1c223abad9695e923c83882ea327823028d50fb8
Wind Gabriel <gabriel.pcmk@hotmail.com>
2024-10-14 19:11:45 +0700

    Init commit for workshop4_1_taskflowAPI_method

---
+++ DataTH_Workshops/Workshop4/start_ws4_exercise1_taskflow.py
@@ -0,0 +1,44 @@
+import datetime
+from airflow.operators.bash import BashOperator
+from airflow.decorators import dag, task
+from airflow.utils.dates import days_ago
+
+default_args = {
+    'owner': 'datath',
+}
+
+# Exercise1: Simple Pipeline - Hello World Airflow!
+# รู้จักกับ Task Flow API ที่มาใหม่ใน Airflow 2.0
+# เป็นวิธีการเขียน DAG แบบใหม่ ที่อ่านง่าย และทันสมัยขึ้น เหมาะสำหรับโค้ดที่เป็น PythonOperator ทั้งหมด
+
+@task()
+def print_hello():
+    """
+    Print Hello World!
+    """
+    print("Hello World!")
+    
+
+@task()
+def print_date():
+    """
+    Print current date
+    ref: https://www.w3schools.com/python/python_datetime.asp
+    """
+    print(datetime.datetime.now())
+
+
+@dag(default_args=default_args, schedule_interval=None, start_date=days_ago(1), tags=['exercise'])
+def exercise1_taskflow_dag():
+
+    t1 = print_hello()
+    t2 = print_date()
+
+    t3 = BashOperator(
+        task_id="list_file_gcs",
+        bash_command ="gsutil ls gs://us-central1-workshop4-e25f0be1-bucket/dags"
+    )    
+    # TODO: สร้าง task dependencies ที่นี่
+    t1 >> (t2,t3)
+
+exercise1_dag = exercise1_taskflow_dag()
