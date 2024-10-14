35ce4a561be3e4b5d21b80f5664d855bcb84f21e
Wind Gabriel <gabriel.pcmk@hotmail.com>
2024-10-14 19:22:00 +0700

    Init commit for workshop4_3

---
+++ DataTH_Workshops/Workshop4/start_ws4_exercise3.py
@@ -0,0 +1,28 @@
+from airflow.models import DAG
+from airflow.operators.dummy import DummyOperator
+from airflow.utils.dates import days_ago
+
+
+with DAG(
+    "exercise3_fan_in_dag",
+    start_date=days_ago(1),
+    schedule_interval=None,
+    tags=["exercise"]
+) as dag:
+
+    # Exercise3: Fan-in Pipeline
+    # ใน exercise นี้จะได้รู้จักการเขียน task ใน pipeline ขั้นตอนเยอะขึ้น
+    # ใช้ DummyOperator เป็น task จำลอง
+ 
+    t0 = DummyOperator(task_id="task_0")
+    t1 = DummyOperator(task_id="task_1")
+    t2 = DummyOperator(task_id="task_2")
+    t3 = DummyOperator(task_id="task_3")
+    t4 = DummyOperator(task_id="task_4")
+    t5 = DummyOperator(task_id="task_5")
+    t6 = DummyOperator(task_id="task_6")
+    
+     
+    # TODO: สร้าง DummyOperator เพื่อสร้าง dependency ที่ซับซ้อน
+    [t0, t1, t2]>>t4
+    [t3, t4, t5]>>t6
