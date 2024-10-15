dde430f795c209d060b024c411c3f28db14557ec
Wind Gabriel <gabriel.pcmk@hotmail.com>
2024-10-15 14:45:20 +0700

    Init commit for workshop5

---
+++ DataTH_Workshops/Workshop5/start_ws5_gcs_to_bq.py
@@ -0,0 +1,200 @@
+from airflow.models import DAG
+from airflow.operators.bash import BashOperator
+from airflow.operators.python import PythonOperator
+from airflow.providers.google.cloud.transfers.gcs_to_bigquery import GCSToBigQueryOperator #มีการ import แล้ว
+from airflow.providers.mysql.hooks.mysql import MySqlHook
+from airflow.utils.dates import days_ago
+import pandas as pd
+import requests
+
+MYSQL_CONNECTION = "mysql_default"   # ชื่อของ connection ใน Airflow ที่เซ็ตเอาไว้
+CONVERSION_RATE_URL = "https://r2de2-workshop-vmftiryt6q-ts.a.run.app/usd_thb_conversion_rate"
+
+# path ที่จะใช้
+mysql_output_path = "/home/airflow/gcs/data/audible_data_merged.csv"
+conversion_rate_output_path = "/home/airflow/gcs/data/conversion_rate.csv"
+final_output_path = "/home/airflow/gcs/data/output.csv"
+
+
+def get_data_from_mysql(transaction_path):
+    # รับ transaction_path มาจาก task ที่เรียกใช้
+
+    # เรียกใช้ MySqlHook เพื่อต่อไปยัง MySQL จาก connection ที่สร้างไว้ใน Airflow
+    mysqlserver = MySqlHook(MYSQL_CONNECTION)
+    
+    # Query จาก database โดยใช้ Hook ที่สร้าง ผลลัพธ์ได้ pandas DataFrame
+    audible_data = mysqlserver.get_pandas_df(sql="SELECT * FROM audible_data")
+    audible_transaction = mysqlserver.get_pandas_df(sql="SELECT * FROM audible_transaction")
+
+    # Merge data จาก 2 DataFrame เหมือนใน workshop1
+    df = audible_transaction.merge(audible_data, how="left", left_on="book_id", right_on="Book_ID")
+
+    # Save ไฟล์ CSV ไปที่ transaction_path ("/home/airflow/gcs/data/audible_data_merged.csv")
+    # จะไปอยู่ที่ GCS โดยอัตโนมัติ
+    df.to_csv(transaction_path, index=False)
+    print(f"Output to {transaction_path}")
+
+
+def get_conversion_rate(conversion_rate_path):
+    r = requests.get(CONVERSION_RATE_URL)
+    result_conversion_rate = r.json()
+    df = pd.DataFrame(result_conversion_rate)
+
+    # เปลี่ยนจาก index ที่เป็น date ให้เป็น column ชื่อ date แทน แล้วเซฟไฟล์ CSV
+    df = df.reset_index().rename(columns={"index": "date"})
+    df.to_csv(conversion_rate_path, index=False)
+    print(f"Output to {conversion_rate_path}")
+
+
+def merge_data(transaction_path, conversion_rate_path, output_path):
+    # อ่านจากไฟล์ สังเกตว่าใช้ path จากที่รับ parameter มา
+    transaction = pd.read_csv(transaction_path)
+    conversion_rate = pd.read_csv(conversion_rate_path)
+
+    transaction['date'] = transaction['timestamp']
+    transaction['date'] = pd.to_datetime(transaction['date']).dt.date
+    conversion_rate['date'] = pd.to_datetime(conversion_rate['date']).dt.date
+
+    # merge 2 DataFrame
+    final_df = transaction.merge(conversion_rate, how="left", left_on="date", right_on="date")
+    
+    # แปลงราคา โดยเอาเครื่องหมาย $ ออก และแปลงให้เป็น float
+    final_df["Price"] = final_df.apply(lambda x: x["Price"].replace("$",""), axis=1)
+    final_df["Price"] = final_df["Price"].astype(float)
+
+    final_df["THBPrice"] = final_df["Price"] * final_df["conversion_rate"]
+    final_df = final_df.drop(["date", "book_id"], axis=1)
+
+    # save ไฟล์ CSV
+    final_df.to_csv(output_path, index=False)
+    print(f"Output to {output_path}")
+    
+
+
+with DAG(
+    "workshop5_gcs_to_bq_dag",
+    start_date=days_ago(1),
+    schedule_interval="@once",
+    tags=["workshop"]
+) as dag:
+
+    dag.doc_md = """
+    # Workshop5: Load to BigQuery ด้วยคำสั่ง GCSToBigQueryOperator ของ Airflow 
+    เหมาะสำหรับ worker ที่ไม่มี bq command
+    """
+
+    t1 = PythonOperator(
+        task_id="get_data_from_mysql",
+        python_callable=get_data_from_mysql,
+        op_kwargs={"transaction_path": mysql_output_path},
+    )
+
+    t2 = PythonOperator(
+        task_id="get_conversion_rate",
+        python_callable=get_conversion_rate,
+        op_kwargs={"conversion_rate_path": conversion_rate_output_path},
+    )
+
+    t3 = PythonOperator(
+        task_id="merge_data",
+        python_callable=merge_data,
+        op_kwargs={
+            "transaction_path": mysql_output_path,
+            "conversion_rate_path": conversion_rate_output_path, 
+            "output_path": final_output_path
+        },
+    )
+
+    # สร้าง t4 ที่เป็น GCSToBigQueryOperator เพื่อใช้งานกับ BigQuery แบบ Airflow และใส่ dependencies
+    t4 = GCSToBigQueryOperator(
+    task_id='gcs_to_bq',
+    bucket='us-central1-workshop4-e25f0be1-bucket',  #มันก็จะเหมือนขั้นตอนที่แล้วแค่เขียนแยก us-central1-workshop4real-cc22592e-bucket/data/output.csv
+    source_objects=['data/output.csv'],
+    destination_project_dataset_table='workshop_5.audible_data2', #ให้ไปลงที่ Big Q ชื่อ workshop5 ชื่อว่า audible_data2
+    skip_leading_rows = 1,
+    schema_fields=[     #ในส่วน Schema_fields นั้นเอามาจากคำสั่ง bq show --schema --format prettyjson workshop5.audible_data
+            {           #โดยทำการ run ใน terminal #ปกติแล้วเคสกรณีจริงเขาจะมีการเก็บเป็น .JSON ให้ จากนั้นเราก็ทำการ Load มาดู
+                "mode": "NULLABLE",
+                "name": "timestamp",
+                "type": "TIMESTAMP"
+            },
+            {
+                "mode": "NULLABLE",
+                "name": "user_id",
+                "type": "STRING"
+            },
+            {
+                "mode": "NULLABLE",
+                "name": "country",
+                "type": "STRING"
+            },
+            {
+                "mode": "NULLABLE",
+                "name": "Book_ID",
+                "type": "INTEGER"
+            },
+            {
+                "mode": "NULLABLE",
+                "name": "Book_Title",
+                "type": "STRING"
+            },
+            {
+                "mode": "NULLABLE",
+                "name": "Book_Subtitle",
+                "type": "STRING"
+            },
+            {
+                "mode": "NULLABLE",
+                "name": "Book_Author",
+                "type": "STRING"
+            },
+            {
+                "mode": "NULLABLE",
+                "name": "Book_Narrator",
+                "type": "STRING"
+            },
+            {
+                "mode": "NULLABLE",
+                "name": "Audio_Runtime",
+                "type": "STRING"
+            },
+            {
+                "mode": "NULLABLE",
+                "name": "Audiobook_Type",
+                "type": "STRING"
+            },
+            {
+                "mode": "NULLABLE",
+                "name": "Categories",
+                "type": "STRING"
+            },
+            {
+                "mode": "NULLABLE",
+                "name": "Rating",
+                "type": "STRING"
+            },
+            {
+                "mode": "NULLABLE",
+                "name": "Total_No__of_Ratings",
+                "type": "FLOAT"
+            },
+            {
+                "mode": "NULLABLE",
+                "name": "Price",
+                "type": "FLOAT"
+            },
+            {
+                "mode": "NULLABLE",
+                "name": "conversion_rate",
+                "type": "FLOAT"
+            },
+            {
+                "mode": "NULLABLE",
+                "name": "THBPrice",
+                "type": "FLOAT"
+            }
+        ],
+    write_disposition='WRITE_TRUNCATE',
+)
+
+    [t1, t2] >> t3 >> t4
